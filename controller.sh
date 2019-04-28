ecbe42f465b20d9df13fa11021f19a5c43c7ce02
godacre <48926225+godacre@users.noreply.github.com>
2019-04-28 22:32:59 +0100

    Replacing controller.sh

---
+++ controller.sh
@@ -0,0 +1,30 @@
+#!/bin/bash
+######################################################
+###                                                ###
+###   Name: controller.sh    Author: J Goodacre    ###
+###                                                ###
+###   Usage: ./controller.sh                       ###
+###                                                ###
+###  A simple controller script to install Ansible ###
+###  on a fresh CentOS 7 machine to then execute   ###
+###  the ansible-playbook and further code.        ###
+###                                                ###
+######################################################
+echo -e " --- Installing Bastion Dependencies --- "
+sudo yum install -q ansible
+sudo yum --enablerepo=extras install -q epel-release
+sudo yum install -y -q python python-dev python-pip
+sudo pip install -q boto boto3
+echo -e "\n --- Dependencies installed --- \n"
+
+echo -e " --- Exporting env variables --- "
+export AWS_ACCESS_KEY_ID=AKIA4BHHCVQ24FEB325O
+export AWS_SECRET_ACCESS_KEY=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
+export EC2_INI_PATH=./inventory/ec2.ini
+export ANSIBLE_INVENTORY=./inventory
+echo -e " --- Variables exported --- \n"
+
+echo -e " --- Executing Playbook --- "
+ansible-playbook run.yml
+echo -e " --- Playbook complete! --- "
+
